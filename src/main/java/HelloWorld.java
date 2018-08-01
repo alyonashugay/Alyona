2cedab571943ed6369718ea89c1a246ac225c386
alyonashugay <alyona.shugay@gmail.com>
2018-08-01 17:57:54 +0300

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,62 @@
+public class HelloWorld {
+    public int myNumber = 5;
+    public double myNumber2 = 3.4;
+    public String myCompany = "Playtika";
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<100; i++){
+            System.out.println(i*i);
+        }
+
+
+
+
+       /* int i = 0;
+        do {
+            System.out.println("Hello " + args[i++] + "!");
+        }
+        while (i < args.length);
+
+       */
+
+
+
+      /*  int i = 0;
+        while(i < args.length) {
+            System.out.println("Hello " + args[i++] + "!");
+
+        }
+        */
+
+   /* switch (args.length) {
+         case 0:
+              System.out.println("Hello Stranger!");
+              break;
+         case 1:
+               System.out.println("Hello " + args[0] + "!");
+               break;
+         case 2:
+          System.out.println("Hello " + args[0] + " " + args[1] + "!");
+             break;
+         default:
+             System.out.println("Hello World");
+      */   }
+
+
+
+
+/*
+        if (args.length != 0) {
+            System.out.println("Hello " + args[0] + "!");
+        } else {
+            System.out.println("Hello Stranger");
+        }
+*/
+    }
+
+
+
+
+
+
