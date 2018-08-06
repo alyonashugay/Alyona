6a764d2e785d8185d01e58c1472f88f71a1d72f0
alyonashugay <alyona.shugay@gmail.com>
2018-08-06 17:23:57 +0300

    Recursion 2

---
+++ src/main/java/Recursion2.java
@@ -0,0 +1,11 @@
+public class Recursion2 {
+    public static void main(String[] args) {
+        System.out.println (fact(4));
+        }
+
+        private static int fact(int n) {
+        if (n==1)
+            return 1;
+        return n*fact(n-1);
+        }
+}
