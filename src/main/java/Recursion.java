2461f28e7b2b7068f188fb8ce613504ad9abcc5a
alyonashugay <alyona.shugay@gmail.com>
2018-08-06 17:21:55 +0300

    Recursion 1

---
+++ src/main/java/Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion {
+    public static void main(String[] args) {
+        counter (3);
+        }
+    private static void counter (int n) {
+        if (n==0)
+            return;
+        System.out.println(n);
+
+     counter (n-1);
+
+    }
+
+}
