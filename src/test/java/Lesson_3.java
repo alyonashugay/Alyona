4b2f34d60c40e641ddd03ea015d98f9ed4ffac2f
alyonashugay <alyona.shugay@gmail.com>
2018-08-07 17:18:40 +0300

    Add String Hello

--- src/test/java/Lesson_3.java
+++ src/test/java/Lesson_3.java
@@ -4,7 +4,7 @@ public class Lesson_3 {
 
         for (int i = 0; i < 100; i++) {
             System.out.println(i * i);
-            System.out.println("Commit");
+            System.out.println("Hello");
         }
     }
 }
