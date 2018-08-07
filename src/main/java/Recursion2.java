4b43c29f6ed94ac58663a9f26d0d9092a7174a04
alyonashugay <alyona.shugay@gmail.com>
2018-08-07 15:59:33 +0300

    Lesson_3

--- src/main/java/Recursion2.java
+++ src/main/java/Recursion2.java
@@ -1,11 +1,11 @@
 public class Recursion2 {
     public static void main(String[] args) {
-        System.out.println (fact(5));
-        }
+        System.out.println(fact(5));
+    }
 
-        private static int fact(int n) {
-        if (n==1)
+    private static int fact(int n) {
+        if (n == 1)
             return 1;
-        return n*fact(n-1);
-        }
+        return n * fact(n - 1);
+    }
 }
